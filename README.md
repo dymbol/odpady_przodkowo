8fc044974bce9ad00c8c8fd9ccc0bb9edbd28769
Bartosz Dempc <dempc.bartosz@gmail.com>
2025-07-25 14:33:09 +0200

    added icons

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Kalendarz wywozu odpadów dla Gminy Przodkowo
-Gmina Przodkowo udosępnia kalendarze wywozu odpaów w formie papierowej oraz pliów pdf. W celu dodania dni wywozu do kalendarzy elektronicznych (np.: Google Calendar) i systemów Automatyzacji, nalży takie dane wprowadzić.
+Gmina Przodkowo udostępnia kalendarze wywozu odpadów w formie papierowej oraz pliów pdf. W celu dodania dni wywozu do kalendarzy elektronicznych (np.: Google Calendar) i systemów Automatyzacji, należy takie dane wprowadzić.
 To repozytorium zawiera informacje:
-- jak zautomatyzowć proces konwersji danych z pliku pdf na formaty Icalendar oraz ICS
-- udostępnia kalendarze w formatach ICalendar i ICS
+- jak zautomatyzowć proces konwersji danych z pliku pdf do formatu [ICS](https://pl.wikipedia.org/wiki/ICalendar)
+- udostępnia kalendarze w formatacie ICS
 
 ## Żródło danych
 Źródłem danych jest oficjalna strona internetowa Gminy Przodkowo: (https://www.przodkowo.pl/index.php?s=wpisy&id=56)
 
 ## Tabela konwersji
-|plik źródłowy|format pośredni|ICS|ICalendar|
-| ------------- |:-------------------:|:-------------------:| -----:|
-|20241212085821_bagniewo_brzeziny_bursztynik_gliniewo_kawle_dolne_kawle_gorne_przodkowo.pdf||||
-|20241212085845_barwik_bielawy_czarna_huta_hejtus_pomieczyno_rab_stanislawy_szarlata_wilanowo.pdf||||
-|20241212085902_czeczewo_hopy_klosowo_klosowko_trzy_rzeki_zaleskie_piaski_zaleze.pdf||||
-|20241212085918_kczewo_kobysewo_kosowo_mlynek_osowa_gora_smoldzino.pdf||||
-|20241212085946_maslowo_nowe_tokary_otalzyno_tokarskie_pnie_tokary_warzenko.pdf|ok|ok||
+|plik źródłowy|ICS|
+| ------------- |:-------------------:|
+|[bagniewo_brzeziny_bursztynik_gliniewo_kawle_dolne_kawle_gorne_przodkowo](data/20241212085821_bagniewo_brzeziny_bursztynik_gliniewo_kawle_dolne_kawle_gorne_przodkowo.pdf)||
+|[barwik_bielawy_czarna_huta_hejtus_pomieczyno_rab_stanislawy_szarlata_wilanowo](data/20241212085845_barwik_bielawy_czarna_huta_hejtus_pomieczyno_rab_stanislawy_szarlata_wilanowo.pdf)||
+|[czeczewo_hopy_klosowo_klosowko_trzy_rzeki_zaleskie_piaski_zaleze](data/20241212085902_czeczewo_hopy_klosowo_klosowko_trzy_rzeki_zaleskie_piaski_zaleze.pdf)||
+|[kczewo_kobysewo_kosowo_mlynek_osowa_gora_smoldzino](data/20241212085918_kczewo_kobysewo_kosowo_mlynek_osowa_gora_smoldzino.pdf)|||
+|[maslowo_nowe_tokary_otalzyno_tokarskie_pnie_tokary_warzenko](data/20241212085946_maslowo_nowe_tokary_otalzyno_tokarskie_pnie_tokary_warzenko.pdf)|[text](data/2025_maslowo_nowe_tokary_otalzyno_tokarskie_pnie_tokary_warzenko_plain.ics), [kolor](data/2025_maslowo_nowe_tokary_otalzyno_tokarskie_pnie_tokary_warzenko_colored.ics)|
 
--- generate_calendars.py
+++ generate_calendars.py
@@ -1,28 +1,52 @@
 from ics import Calendar, Event
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 import data.parsed_data as parsed_data
 
+gmt_plus_2 = timezone(timedelta(hours=2))
+    
+ics_types=["colored","plain"]
 event_types=["BIO","POPIÓŁ","ZMIESZANE","PAPIER","SZKŁO","PLASTIK","TERMIN PŁATNOŚCI"]
+event_emot = {
+    "BIO": "🟫",
+    "POPIÓŁ": "⬜",
+    "ZMIESZANE": "⬛",
+    "PAPIER": "🟦",
+    "SZKŁO": "🟩",
+    "PLASTIK": "🟨",
+    "TERMIN PŁATNOŚCI": "💵",
+}
 
 def convert_date(date, year):
     # format in: DD.MM
     full_date_string = f"{date}.{year}"
-    date_obj = datetime.strptime(full_date_string, "%d.%m.%Y")
+    date_obj = datetime.strptime(full_date_string, "%d.%m.%Y").replace(tzinfo=gmt_plus_2)
     return date_obj
 
 
 for element in parsed_data.data_places:
-    c = Calendar()
-    c.name=f'Harmonogram wywozu odpaów dla miejscowści: {element["miejscowości"]}'
-    c.organizer="Gmina Przodkowo"
+    for ics_type in ics_types:
+        c = Calendar()
+        c.creator='https://github.com/dymbol/odpady_przodkowo'
+        c.name=f'Harmonogram wywozu odpaów dla miejscowści: {element["miejscowości"]}'
+        c.organizer="Gmina Przodkowo"
 
-    with open(f'data/{element["rok"]}_{element["miejscowości"]}.ics',"w",newline="") as my_calendar:
-        for event_type in event_types:
-            for event_date in element[event_type]:
-                e = Event()
-                e.summary = event_type
-                e.description = event_type
-                e.begin = convert_date(event_date,element["rok"])
-                e.all_day: True
-                c.events.add(e)        
-        my_calendar.write(c.serialize())
+        with open(f'data/{element["rok"]}_{element["miejscowości"]}_{ics_type}.ics',"w",newline="") as my_calendar:
+            for event_type in event_types:
+
+                waste_name = ""
+                if ics_type == "colored":
+                    waste_name = f"{event_emot[event_type]} {event_type}"
+                elif ics_type == "plain":
+                    waste_name = event_type
+
+                for event_date in element[event_type]:
+                    e = Event()
+                    e.name = waste_name
+                    e.summary = waste_name
+                    e.description = f" wywóz odpadów: {waste_name}"
+                    e.begin = convert_date(event_date,element["rok"])
+                    e.begin = convert_date(event_date,element["rok"]) + timedelta(hours=7)
+                    e.end = convert_date(event_date,element["rok"]) + timedelta(hours=17)
+                    e.all_day: True
+                    c.events.add(e)        
+            my_calendar.write(c.serialize())
